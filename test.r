bf07a861ad9676835df60adc6e2113c1a23c8fb5
dashedman <assarbsmail@gmail.com>
2020-10-01 23:01:57 +0300

    add table

--- test.r
+++ test.r
@@ -84,6 +84,32 @@ LOO <- function(dat, func, maxK = 1, l = 2){
     return (list(rating=rat, selection=selection))
 }
 
+fast_kwnn_LOO <- function(selection, q=0.9){
+    l = dim(selection)[1]
+    rat = rep(0, l-1)
+    w <- rep(0, l)
+    for(i in 1:l){
+      w[i] = q^i
+    }
+
+    for(i in 1:l){
+        dat = selection[-i,]
+        point = selection[i,]
+
+        n <- dim(dat)[2] - 1
+        orderedDat <- dist_sort(dat, point[1:n], euclid_distance)
+        classes = rep(0, length(levels(orderedDat[, n+1])))
+
+        for(k in 1:(l-1)){
+            classes[orderedDat[k, n+1]] = classes[orderedDat[k, n+1]] + w[k]
+            class <- levels(orderedDat[, n+1])[which.max(classes)]
+            if(point[1, n+1] != class){
+                rat[k] = rat[k] + 1
+            }
+        }
+    }
+    return (list(rating=rat, selection=selection))
+}
 ####################################
 .random_1NN <- function(){
     #1NN calc
@@ -120,7 +146,7 @@ LOO <- function(dat, func, maxK = 1, l = 2){
 #calc 6NN map
 .map_6NN <- function(){
     print("map")
-    png(paste0("6NN_map_plot", ".png"), width = 1080, height = 540)
+    png(paste0("6NN_map_plot", ".png"), width = 540, height = 270)
     plot(
       iris[,3:4],
       pch = 21,
@@ -197,9 +223,19 @@ LOO <- function(dat, func, maxK = 1, l = 2){
       asp = T,
       main = "Classification map for 5wNN"
     )
-    for(x in seq(1,8,0.1)){
-        for(y in seq(0.1,4.5,0.1)){
-            points(x,y,pch=1,col = colors[kwNN(result$selection[, 1:3], c(x,y), k=5)])
+    x = seq(
+      result$selection[which.min(result$selection[, 1]), 1],
+      result$selection[which.max(result$selection[, 1]), 1],
+      0.1
+    )
+    y = seq(
+      result$selection[which.min(result$selection[, 2]), 2],
+      result$selection[which.max(result$selection[, 2]), 2],
+      0.1
+    )
+    for(xi in x){
+        for(yi in y){
+            points(xi,yi,pch=1,col = colors[kwNN(result$selection[, 1:3], c(xi,yi), k=5)])
         }
     }
     dev.off()
@@ -215,6 +251,110 @@ LOO <- function(dat, func, maxK = 1, l = 2){
 }
 
 
+.BIG_LOO <- function(){
+    print("q=1")
+    result = fast_kwnn_LOO(iris[, 3:5], q=1)
+    png(paste0("big_LOO", ".png"), width = 1920, height = 1080)
+    par(bg = "#f0ffff")
+    plot(
+      data.frame(k=1:length(result$rating), LOO=result$rating),
+      type="l",
+      col="red",
+      ylim = c(0, dim(result$selection)[1]),
+      main = "Leave One Out estimate for kwNN algorithm"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "red",
+      bg = "red"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+
+    print("q=0.9")
+    result = fast_kwnn_LOO(iris[, 3:5], q=0.99999)
+    lines(
+      1:length(result$rating),
+      y=result$rating,
+      col="darkorchid4"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "darkorchid4",
+      bg = "darkorchid4"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+
+    print("q=0.7")
+    result = fast_kwnn_LOO(iris[, 3:5], q=0.9999)
+    lines(
+      1:length(result$rating),
+      y=result$rating,
+      col="deepskyblue4"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "deepskyblue4",
+      bg = "deepskyblue4"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+
+    print("q=0.5")
+    result = fast_kwnn_LOO(iris[, 3:5], q=0.999)
+    lines(
+      1:length(result$rating),
+      y=result$rating,
+      col="darkseagreen"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "darkseagreen",
+      bg = "darkseagreen"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+
+    print("q=0.3")
+    result = fast_kwnn_LOO(iris[, 3:5], q=0.99)
+    lines(
+      1:length(result$rating),
+      y=result$rating,
+      col="forestgreen"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "forestgreen",
+      bg = "forestgreen"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+
+    print("q=0.1")
+    result = fast_kwnn_LOO(iris[, 3:5], q=0.9)
+    lines(
+      1:length(result$rating),
+      y=result$rating,
+      col="lawngreen"
+    )
+    points(
+      which.min(result$rating),
+      result$rating[which.min(result$rating)],
+      pch = 21,
+      col = "lawngreen",
+      bg = "lawngreen"
+    )
+    print(which.min(result$rating));print(result$rating[which.min(result$rating)])
+    dev.off()
+
+}
+
 
 
 ######################################
@@ -225,7 +365,8 @@ colors <- c(
     "virginica" = "purple"
 )
 
-.random_1NN()
+#.random_1NN()
 #.map_6NN()
 #.LOO_15_20()
 #.LOO_map_w_15_20()
+.BIG_LOO()
